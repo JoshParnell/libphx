4610faa893693439d7acbcf67bd12b6f0f5fd07f
JoshParnell <josh@ltheory.com>
2022-07-24 14:17:32 -0400

    build: factor out common target property setting to shared fn

--- script/build/Shared.cmake
+++ script/build/Shared.cmake
@@ -32,3 +32,41 @@ function (phx_configure_output_dir target)
       ARCHIVE_OUTPUT_DIRECTORY_${config} "${CMAKE_SOURCE_DIR}/bin")
   endforeach (config)
 endfunction ()
+
+function (phx_configure_target_properties target)
+  if (WINDOWS)
+    target_compile_definitions (${target} PRIVATE _CRT_SECURE_NO_DEPRECATE)
+    target_compile_definitions (${target} PRIVATE WIN32_LEAN_AND_MEAN)
+    target_compile_definitions (${target} PRIVATE WINDOWS=1)
+
+    target_compile_options (${target} PRIVATE "/MP")         # Multithreaded Build
+    target_compile_options (${target} PRIVATE "/MD")         # Dynamic C Runtime
+    target_compile_options (${target} PRIVATE "/EHs-c-")     # No exception handling
+    target_compile_options (${target} PRIVATE "/fp:fast")    # No strict FP
+    target_compile_options (${target} PRIVATE "/GL")         # Whole Program Optimization
+    target_compile_options (${target} PRIVATE "/GS-")        # No Buffer Security Checks
+    target_compile_options (${target} PRIVATE "/GR-")        # No RTTI
+    target_compile_options (${target} PRIVATE "/arch:SSE2")  # Assume SSE2+
+  elseif (LINUX)
+    target_compile_definitions (${target} PRIVATE UNIX=1)
+
+    target_compile_options (${target} PRIVATE "-Wall")            # All error checking
+    target_compile_options (${target} PRIVATE "-fno-exceptions")  # No exception handling
+    target_compile_options (${target} PRIVATE "-ffast-math")      # No strict FP
+    target_compile_options (${target} PRIVATE "-fpic")            # PIC since this is shared
+
+    target_compile_options (${target} PRIVATE "-Wno-unused-variable")
+    target_compile_options (${target} PRIVATE "-Wno-unknown-pragmas")
+
+    # Aggressive optimization, assuming SSE4+
+    target_compile_options (${target} PRIVATE "-O3")
+    target_compile_options (${target} PRIVATE "-msse")
+    target_compile_options (${target} PRIVATE "-msse2")
+    target_compile_options (${target} PRIVATE "-msse3")
+    target_compile_options (${target} PRIVATE "-msse4")
+
+    # :(
+    target_compile_options (${target} PRIVATE "-std=c++11")
+
+  endif ()
+endfunction ()
